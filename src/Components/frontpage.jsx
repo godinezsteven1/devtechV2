bea029314e491569c15c7541c38ad1cbe5ba5126
godinezsteven1 <steven.godin.42@gmail.com>
2025-06-04 23:09:07 -0500

    added blink animation

--- src/Components/frontpage.jsx
+++ src/Components/frontpage.jsx
@@ -6,11 +6,12 @@ export default function FrontPage() {
   return (
     <section className="min-h-screen flex flex-col items-center justify-center px-6 text-center">
       <h1 className="mt-[-15rem] text-4xl sm:text-5xl md:text-6xl font-extrabold leading-tight text-[#c792ea] drop-shadow-[0_0_6px_#c792ea]">
-        MACHINE<br />LEARNING<br />ENGINEER
+        MACHINE<br />LEARNING<br />ENGINEER 
+        {/* am i craazy or do these not line up  */}
       </h1>
 
-      <p className="mt-6 max-w-xl text-md sm:text-lg text-purple-300 leading-relaxed tracking-wide">
-        Building the future with code, intelligence, and creativity.
+      <p className="mt-6 max-w-xl text-xl text-purple-300 leading-relaxed tracking-wide">
+        Building the future with code, intelligence, and creativity.<span className="blinking-cursor">|</span>
       </p>
 
       <div className="mt-8 flex flex-wrap gap-4 justify-center">
