5c9d4b07f31c6a0932b5dcd7ae0ff8ef3cc51978
godinezsteven1 <steven.godin.42@gmail.com>
2025-06-24 21:47:14 -0500

    added resume

--- src/Components/frontpage.jsx
+++ src/Components/frontpage.jsx
@@ -35,17 +35,34 @@ export default function FrontPage() {
         </a>
       </div>
       
+
       <div className="mt-4 w-full flex justify-center">
-  <div className="w-full max-w-md flex justify-center md:justify-center md:mr-5">
-    <Link
-      to="/resume"
-      className="border border-purple-400 text-purple-200 px-6 py-2 rounded-md hover:bg-purple-400 hover:text-black transition duration-300 font-semibold tracking-wide 
-      hover:animate-pulse md:w-33 md:h-10"
-    >
-      RESUME
-    </Link>
-  </div>
-</div>
+        <div className="w-full max-w-md flex justify-center md:justify-center md:mr-5">
+          <a
+            href="/static/resume/GodinezAI2025.pdf" 
+            download
+            className="border border-purple-400 text-purple-200 px-6 py-2 rounded-md hover:bg-purple-400 hover:text-black transition duration-300 font-semibold tracking-wide hover:animate-pulse md:w-33 md:h-10 flex items-center justify-center gap-2"
+          >
+            <span>RESUME</span>
+            <svg 
+              xmlns="http://www.w3.org/2000/svg" 
+              className="h-10 w-10" 
+              fill="none" 
+              viewBox="0 0 24 24" 
+              stroke="currentColor"
+              >
+                <path 
+                strokeLinecap="round" 
+                strokeLinejoin="round" 
+                strokeWidth={2} 
+                d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" 
+              />
+            </svg>
+          </a>
+        </div>
+      </div>
+
+
     </section>
   );
 }
