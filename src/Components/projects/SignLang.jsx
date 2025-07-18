e950661028a87ff4bb93afe4dbbfede488a4b799
godinezsteven1 <steven.godin.42@gmail.com>
2025-07-17 22:33:29 -0500

    #7 fixed

--- src/Components/projects/SignLang.jsx
+++ src/Components/projects/SignLang.jsx
@@ -54,12 +54,12 @@ export default function SignLang() {
                                         <span className="mr-2">🎯</span>Our Mission
                                     </h3>
                                     <p className="text-xl leading-relaxed font-medium text-gray-100">
-                                    The project aims to create adaptive accesability technology for the deaf and hard of hearing communities.
+                                    The project aims to create adaptive accessibility technology for the deaf and hard of hearing communities.
                                     </p>
                                 </div>
                                 <p className="text-lg leading-relaxed">
                                     This project utilizes a Convolutional Neural Network (CNN) to store datasets of American Sign Language (ASL),
-                                    German Sign Language (DGS), and Spanish Sign Language (LSE). using a unified deep learning pipeline.
+                                    German Sign Language (DGS), and Spanish Sign Language (LSE), using a unified deep learning pipeline.
                                     Leveraging a convolutional neural network (CNN) trained on a custom-curated and standardized dataset,
                                     the system integrates MediaPipe for real-time hand tracking, TensorFlow for gesture classification, and
                                     natural language processing (NLP) for output refinement and translation. By accommodating overlapping 
@@ -99,22 +99,23 @@ export default function SignLang() {
                                 </a>
                             </p>
 
-                            The nature of a Natural Language Processing pipeline is to process human 
-                            language and in our case mimic its structure. Initially, I believed a Hidden Markov 
-                            Model would fit most appropriately as they are modeled to find the "probabilistic
-                            relationship between a sequence of observations and a sequence of hidden states". 
-                            However, due to the inclusion and complexity of day to day conversation. A model that assumes
-                            each emission, observed word / token, is independent of the rest lacks to capture 
-                            transitions states between words or better known as, context. My solution was to 
-                            transition the classic HMM to a Bigram Hidden Markov Model. A Bigram Hidden Markov
-                            models introduces dependencies to the previous observation, that help capture word state
-                            transitions in a sequence, while eliminating emissions; each word its is own state and our 
-                            algorithm tracks transition probabilities between words. This alongside with its limitations, 
-                            gave a solution to state transitions that is able to keep context as the most probable 
-                            next word sequence using a Viterbi algorithm that has a faster runtime. However, the BHMM is
-                            still limited as it cannot accurately capture long term sequences with out it being 
-                            computationally expensive where it slows down outputs, ruining the quality of the software.
-                    
+                            <p className="text-lg leading-relaxed">
+                                The nature of a Natural Language Processing pipeline is to process human 
+                                language and in our case mimic its structure. Initially, I believed a Hidden Markov 
+                                Model would fit most appropriately as they are modeled to find the "probabilistic
+                                relationship between a sequence of observations and a sequence of hidden states". 
+                                However, due to the inclusion and complexity of day-to-day conversation, a model that assumes
+                                each emission, observed word / token, is independent of the rest lacks to capture 
+                                transitions states between words or better known as, context. My solution was to 
+                                transition the classic HMM to a Bigram Hidden Markov Model. A Bigram Hidden Markov
+                                model introduces dependencies to the previous observation, that help capture word state
+                                transitions in a sequence, while eliminating emissions; each word is its own state and our 
+                                algorithm tracks transition probabilities between words. This alongside with its limitations, 
+                                gave a solution to state transitions that is able to keep context as the most probable 
+                                next word sequence using a Viterbi algorithm that has a faster runtime. However, the BHMM is
+                                still limited as it cannot accurately capture long term sequences without it being 
+                                computationally expensive where it slows down outputs, ruining the quality of the software.
+                            </p>
                         </div>
                     </div>
                 </div>
